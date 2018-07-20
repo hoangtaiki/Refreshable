714b2637f4ae30ad6c390d0f8fe363ee3e49b4cd
Harry.Tran <duchoang.vp@gmail.com>
2018-07-20 15:14:49 +0700

    Initialize project

---
+++ Source/Refreshable.h
@@ -0,0 +1,19 @@
+//
+//  Refreshable.h
+//  Refreshable
+//
+//  Created by Hoangtaiki on 7/20/18.
+//  Copyright © 2018 toprating. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Refreshable.
+FOUNDATION_EXPORT double RefreshableVersionNumber;
+
+//! Project version string for Refreshable.
+FOUNDATION_EXPORT const unsigned char RefreshableVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Refreshable/PublicHeader.h>
+
+
